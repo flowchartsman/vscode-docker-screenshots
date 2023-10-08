bd51242c5e7bb81642a2792a018263f366f86960
Andy Walker <andy@andy.dev>
2023-10-07 22:21:42 -0400

    slightly more responsive-feeling wait message

--- scripts/waitvscode.py
+++ scripts/waitvscode.py
@@ -5,21 +5,21 @@ import sys
 import time
 
 if __name__ == '__main__':
-    print("Waiting for vscode.",end="")
+    print("Waiting for vscode.",end="",flush=True)
     for _ in range(10):
         try:
             focused = Xlib.display.Display().get_input_focus().focus
             if isinstance(focused, int):
-                print(".", end ="")
+                print(".", end ="",flush=True)
                 time.sleep(1)
                 continue
             app = focused.get_wm_class()[0]
             if app == "code":
                 time.sleep(3)
-                print("done")
+                print("done",flush=True)
                 sys.exit(0)
         except AttributeError:
-            print(".", end ="")
+            print(".", end ="",flush=True)
             time.sleep(1)
             continue
     print("timed out")
